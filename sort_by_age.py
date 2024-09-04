d09d747ff65f250d7fa908b9b49c739f5c762444
SPIDER <162958422+spidervsyou@users.noreply.github.com>
2024-09-04 12:38:38 +0300

    Update sort_by_age.py

--- sort_by_age.py
+++ sort_by_age.py
@@ -1,3 +1,9 @@
-def sort_by_age(persons):
-    """Sorts a list of tuples by age in ascending order."""
-    return sorted(persons, key=lambda x: x[1])
+def sort_by_age(tuples_list):
+    # Sort tuples by age (second element)
+    return sorted(tuples_list, key=lambda x: x[1])
+
+# Example
+if __name__ == "__main__":
+    data = [("niki", 31), ("jake", 24), ("mike", 36)]
+    sorted_data = sort_by_age(data)
+    print("Sorted by age:", sorted_data)
